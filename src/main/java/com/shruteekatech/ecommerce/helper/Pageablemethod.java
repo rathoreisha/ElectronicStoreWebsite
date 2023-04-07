829128c2ed10da96953acdf45befa72df5ffb1a8
Isha <isharathore345@gmail.com>
2023-04-07 16:32:58 +0530

    #13-Helper pagable added

---
+++ src/main/java/com/shruteekatech/ecommerce/helper/Pageablemethod.java
@@ -0,0 +1,26 @@
+package com.shruteekatech.ecommerce.helper;
+
+
+import com.shruteekatech.ecommerce.dtos.PagableResponse;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Pageablemethod {
+
+    public static  <U,V> PagableResponse<V> getPageableresponse(Page<U> page, Class<V> type){
+        List<U> users = page.getContent();
+        List<V> userDtoList = users.stream().map(object -> new ModelMapper().map(object,type)).collect(Collectors.toList());
+
+        PagableResponse<V> pagableResponse=new PagableResponse();
+        pagableResponse.setContent(userDtoList);
+        pagableResponse.setPageNumber(page.getNumber());
+        pagableResponse.setPageSize(page.getSize());
+        pagableResponse.setLastPage(page.isLast());
+        pagableResponse.setTotalPages(page.getTotalPages());
+        pagableResponse.setTotalElement(page.getTotalElements());
+        return pagableResponse;
+    }
+}
