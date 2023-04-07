b21a59dccab756dbbca55c15a8ffa6a8f658da59
Isha <isharathore345@gmail.com>
2023-04-07 16:35:50 +0530

    -Category service added

---
+++ src/main/java/com/shruteekatech/ecommerce/service/CategoryService.java
@@ -0,0 +1,32 @@
+package com.shruteekatech.ecommerce.service;
+
+
+
+import com.shruteekatech.ecommerce.dtos.CategoryDto;
+import com.shruteekatech.ecommerce.dtos.PagableResponse;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public interface CategoryService {
+
+//    Create
+    CategoryDto createCategory(CategoryDto categoryDto);
+
+//    Update
+    CategoryDto updateCategory(CategoryDto Category,Long catId);
+
+//    GetSinglecategory
+    CategoryDto getSingleCategory(Long catID);
+
+//    GetAll Category
+    PagableResponse<CategoryDto> getAllcategories(Integer pagenumber, Integer pagesize, String sortBy, String sortDir);
+
+//    Search Category
+    List<CategoryDto> searchCategory(String keyword);
+
+//    Delete Category
+    void deleteCategory(Long catid);
+
+   // String exportrept(String reportformat) throws FileNotFoundException, JRException;
+}
