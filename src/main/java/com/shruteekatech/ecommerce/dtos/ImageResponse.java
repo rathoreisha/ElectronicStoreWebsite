51d9b5e0365591e48ba3193113ca49cd786c582c
Isha <isharathore345@gmail.com>
2023-04-07 16:28:11 +0530

    #10-ImageResponse Added

---
+++ src/main/java/com/shruteekatech/ecommerce/dtos/ImageResponse.java
@@ -0,0 +1,17 @@
+package com.shruteekatech.ecommerce.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageResponse {
+
+    private String imagename;
+    private String message;
+    private boolean status;
+}
