1be4457b0a1edc658b4b6ea6aaea3b7b9a4fe609
Isha <isharathore345@gmail.com>
2023-04-07 15:06:17 +0530

    First Commit

---
+++ src/main/java/com/shruteekatech/ecommerce/ElectronicStoredApplication.java
@@ -0,0 +1,13 @@
+package com.shruteekatech.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicStoredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicStoredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shruteekatech/ecommerce/ElectronicStoredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shruteekatech.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicStoredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
