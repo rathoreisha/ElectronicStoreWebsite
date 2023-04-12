486976f93ea2819b96ca67d990c7602267196b0e
Isha <isharathore345@gmail.com>
2023-04-13 00:32:51 +0530

    #2 BaseTest added

---
+++ src/test/java/com/shruteekatech/ecommerce/BaseTest.java
@@ -0,0 +1,11 @@
+package com.shruteekatech.ecommerce;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+public abstract class BaseTest {
+}
