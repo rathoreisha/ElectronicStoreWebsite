61e14f794670ad8f0edd2de68f55e869864aae0a
Isha <isharathore345@gmail.com>
2023-04-13 00:37:40 +0530

    -no changes

--- src/main/java/com/shruteekatech/ecommerce/ElectronicStoredApplication.java
+++ src/main/java/com/shruteekatech/ecommerce/ElectronicStoredApplication.java
@@ -3,11 +3,14 @@ package com.shruteekatech.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class ElectronicStoredApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElectronicStoredApplication.class, args);
 	}
+
 
 }
